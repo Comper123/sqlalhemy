5acaf330214b2d69e45d53f644559913fb81a664
Comper123 <92387091+Comper123@users.noreply.github.com>
2024-03-31 02:44:46 +0300

    Add files via upload

---
+++ test_resp.py
@@ -0,0 +1,17 @@
+import requests
+
+
+# Получение всех работ
+# print(requests.get('http://127.0.0.1:5000/api/jobs').json())
+
+
+# Корректное получение одной работы
+# print(requests.get('http://127.0.0.1:5000/api/jobs/0').json())
+
+
+# Ошибочный запрос на получение одной работы — неверный id
+# print(requests.get('http://127.0.0.1:5000/api/jobs/1').json())
+
+
+# Ошибочный запрос на получение одной работы — строка
+print(requests.get('http://127.0.0.1:5000/api/jobs/привет').json())
