86c045f3c2c1f8d7b589d4d5d5c2f3791b0b8362
Comper123 <92387091+Comper123@users.noreply.github.com>
2024-03-31 11:04:57 +0300

    Add files via upload

--- api.py
+++ api.py
@@ -2,14 +2,17 @@ import flask
 from data import db_session
 from data.jobs import Jobs
 from flask import jsonify, make_response, request
+from data.users import User
+
 
 blueprint = flask.Blueprint(
-    'news_api',
+    'api',
     __name__,
     template_folder='templates'
 )
 
 
+# ! api модели Jobs
 @blueprint.route('/api/jobs')
 def get_jobs():
     session = db_session.create_session()
@@ -26,7 +29,7 @@ def get_jobs():
 
 
 @blueprint.route('/api/jobs/<int:jobs_id>', methods=['GET'])
-def get_one_news(jobs_id):
+def get_one_job(jobs_id):
     db_sess = db_session.create_session()
     job = db_sess.query(Jobs).get(jobs_id)
     if not job:
@@ -89,5 +92,100 @@ def update_job(jobs_id):
         job.is_finished = request.json['is_finished']
     if 'team_leader' in request.json:
         job.team_leader = request.json['team_leader']
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
+
+
+# ! api модели User
+@blueprint.route('/api/users')
+def get_users():
+    session = db_session.create_session()
+    users = session.query(User).all()
+    return jsonify(
+        {
+            'users': [u.to_dict(only=('surname', 
+                                     'name', 
+                                     'age',
+                                     'position',
+                                     'speciality',
+                                     'address',
+                                     'email')) for u in users]
+        }
+    )
+
+
+@blueprint.route('/api/users/<int:user_id>', methods=['GET'])
+def get_one_user(user_id):
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).get(user_id)
+    if not user:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    return jsonify(
+        {
+            'user': user.to_dict(only=('surname', 
+                                     'name', 
+                                     'age',
+                                     'position',
+                                     'speciality',
+                                     'address',
+                                     'email'))
+        }
+    )
+
+
+@blueprint.route('/api/users', methods=['POST'])
+def create_user():
+    if not request.json:
+        return make_response(jsonify({'error': 'Empty request'}), 400)
+    elif not all(key in request.json for key in
+                 ['surname', 'name', 'age', 'position', 'speciality',
+                  'address', 'email']):
+        return flask.make_response(flask.jsonify({'error': 'Bad request'}), 400)
+    db_sess = db_session.create_session()
+    user = User(
+        surname=request.json['surname'],
+        name=request.json['name'],
+        age=request.json['age'],
+        position=request.json['position'],
+        speciality=request.json['speciality'],
+        address=request.json['address'],
+        email=request.json['email']
+    )
+    db_sess.add(user)
+    db_sess.commit()
+    return jsonify({'id': user.id})
+
+
+@blueprint.route('/api/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).get(user_id)
+    if not user:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    db_sess.delete(user)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
+
+
+@blueprint.route('/api/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).get(user_id)
+    if not user:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    if 'surname' in request.json:
+        user.surname = request.json['surname']
+    if 'name' in request.json:
+        user.name = request.json['name']
+    if 'age' in request.json:
+        user.age = request.json['age']
+    if 'position' in request.json:
+        user.position = request.json['position']
+    if 'speciality' in request.json:
+        user.speciality = request.json['speciality']
+    if 'address' in request.json:
+        user.address = request.json['address']
+    if 'email' in request.json:
+        user.email = request.json['email']
     db_sess.commit()
     return jsonify({'success': 'OK'})
